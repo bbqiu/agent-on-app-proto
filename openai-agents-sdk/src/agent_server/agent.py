2ba09ba9794162b4f2461eb43fd3e46230ecaa8c
Bryan Qiu <bryan.qiu@databricks.com>
2025-10-27 18:22:55 -0400

    working with databricks chat completions endpoints
    Signed-off-by: Bryan Qiu <bryan.qiu@databricks.com>

--- openai-agents-sdk/src/agent_server/agent.py
+++ openai-agents-sdk/src/agent_server/agent.py
@@ -1,56 +1,29 @@
 import asyncio
-import os
 from typing import AsyncGenerator, List, Optional
 
 import mlflow
-from agents import Agent, Runner, set_default_openai_client
+from agents import Agent, Runner, set_default_openai_api, set_default_openai_client
 from agents.mcp import MCPServerStreamableHttp, MCPServerStreamableHttpParams
 from databricks.sdk import WorkspaceClient
-from httpx import AsyncClient, Auth, Request
 from mlflow.types.responses import (
     ResponsesAgentRequest,
     ResponsesAgentResponse,
     ResponsesAgentStreamEvent,
 )
-from openai import AsyncOpenAI
 
-from agent_server.mcp_manager import MCPServerManager
 from agent_server.server import get_obo_workspace_client, invoke, stream
+from agent_server.utils import (
+    MCPServerManager,
+    get_async_openai_client,
+    get_databricks_host_from_env,
+)
 
 sp_workspace_client = WorkspaceClient()
-# openai_client = sp_workspace_client.serving_endpoints.get_open_ai_client()
 user_workspace_client = get_obo_workspace_client()
-
-
-def get_databricks_host_from_env() -> Optional[str]:
-    host = os.getenv("DATABRICKS_HOST")
-    if host is None or host.startswith("https://"):
-        print(host)
-        return host
-    return f"https://{host}"
-
-
-# def _get_async_http_client(workspace_client: WorkspaceClient) -> AsyncClient:
-#     class BearerAuth(Auth):
-#         def __init__(self, get_headers_func):
-#             self.get_headers_func = get_headers_func
-
-#         def auth_flow(self, request: Request) -> Request:
-#             auth_headers = self.get_headers_func()
-#             request.headers["Authorization"] = auth_headers["Authorization"]
-#             yield request
-
-#     databricks_token_auth = BearerAuth(workspace_client.config.authenticate)
-#     return AsyncClient(auth=databricks_token_auth)
-
-
-# openai_client = AsyncOpenAI(
-#     base_url=f"{get_databricks_host_from_env()}/serving-endpoints",
-#     api_key="no-token",  # Passing in a placeholder to pass validations, this will not be used
-#     http_client=_get_async_http_client(sp_workspace_client),
-# )
-
-# set_default_openai_client(openai_client)
+databricks_openai_client = get_async_openai_client(sp_workspace_client)
+set_default_openai_client(databricks_openai_client)
+set_default_openai_api("chat_completions")
+mlflow.openai.autolog()
 
 
 mcp_manager = MCPServerManager()
@@ -67,11 +40,9 @@ mcp_server = mcp_manager.register_server(
 agent = Agent(
     name="code execution agent",
     instructions="You are a code execution agent. You can execute code and return the results.",
-    model="gpt-5-nano",
+    model="gpt-5",
     mcp_servers=[mcp_server],
 )
-
-mlflow.openai.autolog()
 
 
 @invoke()
@@ -84,15 +55,7 @@ async def invoke(request: dict) -> ResponsesAgentResponse:
 # @stream()
 # async def stream(request: dict) -> AsyncGenerator[ResponsesAgentStreamEvent, None]:
 #     async with mcp_manager:
-#         # Extract the user input from the responses API request
-#         input_messages = request.get("input", [])
-#         if input_messages:
-#             # Get the last user message
-#             user_content = input_messages[-1].get("content", "")
-#         else:
-#             user_content = "Hello"
-
-#         result = await Runner.run(agent, user_content)
+#         result = await Runner.run(agent, request.get("input", []))
 
 #         # Yield the response in streaming format
 #         yield {"output": [{"role": "assistant", "content": result.final_output}]}
--- openai-agents-sdk/src/agent_server/utils.py
+++ openai-agents-sdk/src/agent_server/utils.py
@@ -1,9 +1,13 @@
-# from https://github.com/openai/openai-agents-python/issues/1881
-from typing import List
+import os
+from typing import List, Optional
 
 from agents.mcp import MCPServerStdio
+from databricks.sdk import WorkspaceClient
+from httpx import AsyncClient, Auth, Request
+from openai import AsyncOpenAI
 
 
+# from https://github.com/openai/openai-agents-python/issues/1881
 class MCPServerManager:
     """
     Manages the lifecycle of stdio MCP servers for multi-agent workflows.
@@ -59,3 +63,33 @@ class MCPServerManager:
         """Explicitly cleanup all registered servers."""
         for server in self.servers:
             await server.cleanup()
+
+
+def get_databricks_host_from_env() -> Optional[str]:
+    host = os.getenv("DATABRICKS_HOST")
+    if host is None or host.startswith("https://"):
+        print(host)
+        return host
+    return f"https://{host}"
+
+
+def _get_async_http_client(workspace_client: WorkspaceClient) -> AsyncClient:
+    class BearerAuth(Auth):
+        def __init__(self, get_headers_func):
+            self.get_headers_func = get_headers_func
+
+        def auth_flow(self, request: Request) -> Request:
+            auth_headers = self.get_headers_func()
+            request.headers["Authorization"] = auth_headers["Authorization"]
+            yield request
+
+    databricks_token_auth = BearerAuth(workspace_client.config.authenticate)
+    return AsyncClient(auth=databricks_token_auth)
+
+
+def get_async_openai_client(workspace_client: WorkspaceClient) -> AsyncOpenAI:
+    return AsyncOpenAI(
+        base_url=f"{get_databricks_host_from_env()}/serving-endpoints",
+        api_key="no-token",  # Passing in a placeholder to pass validations, this will not be used
+        http_client=_get_async_http_client(workspace_client),
+    )
