9c744f41bb3cb58df6715238d27466bcbe44892f
Bryan Qiu <bryan.qiu@databricks.com>
2025-10-28 16:27:20 -0400

    .
    Signed-off-by: Bryan Qiu <bryan.qiu@databricks.com>

--- langgraph-responses-api-agent/src/agent_server/agent.py
+++ langgraph-responses-api-agent/src/agent_server/agent.py
@@ -1,71 +1,76 @@
 from typing import AsyncGenerator
 
 import mlflow
-from agents import Agent, Runner, set_default_openai_api, set_default_openai_client
-from agents.mcp import MCPServerStdio, MCPServerStreamableHttp, MCPServerStreamableHttpParams
 from databricks.sdk import WorkspaceClient
+from databricks_langchain import ChatDatabricks
+from langchain.agents import create_agent
+from langchain.messages import AIMessageChunk
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from mlflow.types.responses import (
     ResponsesAgentRequest,
     ResponsesAgentResponse,
     ResponsesAgentStreamEvent,
+    create_text_delta,
+    output_to_responses_items_stream,
+    to_chat_completions_input,
 )
 
 from agent_server.server import get_obo_workspace_client, invoke, stream
-from agent_server.utils import get_async_openai_client, get_databricks_host_from_env
+from agent_server.utils import get_databricks_host_from_env
 
+mlflow.langchain.autolog()
 sp_workspace_client = WorkspaceClient()
-# NOTE: this will work for all databricks models *other* than GPT-OSS, which uses a slightly different API
-databricks_openai_client = get_async_openai_client(sp_workspace_client)
-set_default_openai_client(databricks_openai_client)
-set_default_openai_api("chat_completions")
-mlflow.openai.autolog()
 
 
-async def init_mcp_server():
-    return MCPServerStreamableHttp(
-        params=MCPServerStreamableHttpParams(
-            url=f"{get_databricks_host_from_env()}/api/2.0/mcp/functions/system/ai",
-            headers=sp_workspace_client.config.authenticate(),
-        ),
-        client_session_timeout_seconds=20,
-        name="system.ai uc function mcp server",
+def init_mcp_client(workspace_client: WorkspaceClient) -> MultiServerMCPClient:
+    return MultiServerMCPClient(
+        {
+            "system.ai": {
+                "transport": "streamable_http",
+                "url": f"{get_databricks_host_from_env()}/api/2.0/mcp/functions/system/ai",
+                "headers": workspace_client.config.authenticate(),
+            },
+        }
     )
 
 
-def create_coding_agent(mcp_server: MCPServerStdio) -> Agent:
-    return Agent(
-        name="code execution agent",
-        instructions="You are a code execution agent. You can execute code and return the results.",
-        model="databricks-claude-3-7-sonnet",
-        mcp_servers=[mcp_server],
-    )
+async def init_agent():
+    mcp_client = init_mcp_client(sp_workspace_client)
+    tools = await mcp_client.get_tools()
+    return create_agent(tools=tools, model=ChatDatabricks(endpoint="databricks-claude-3-7-sonnet"))
 
 
 @invoke()
-async def invoke(request: ResponsesAgentRequest) -> ResponsesAgentResponse:
-    # user_workspace_client = get_obo_workspace_client()
-    async with await init_mcp_server() as mcp_server:
-        agent = create_coding_agent(mcp_server)
-        messages = [i.model_dump() for i in request.input]
-        result = await Runner.run(agent, messages)
-        return ResponsesAgentResponse(output=[item.to_input_item() for item in result.new_items])
+async def non_streaming(request: ResponsesAgentRequest) -> ResponsesAgentResponse:
+    # obo_workspace_client = get_obo_workspace_client()
+    agent = await init_agent()
+    messages = {"messages": to_chat_completions_input([i.model_dump() for i in request.input])}
+
+    result = await agent.ainvoke(input=messages)
+    return result
 
 
 @stream()
-async def stream(request: dict) -> AsyncGenerator[ResponsesAgentStreamEvent, None]:
-    # user_workspace_client = get_obo_workspace_client()
-    async with await init_mcp_server() as mcp_server:
-        agent = create_coding_agent(mcp_server)
-        messages = [i.model_dump() for i in request.input]
-        result = Runner.run_streamed(agent, input=messages)
+async def streaming(
+    request: ResponsesAgentRequest,
+) -> AsyncGenerator[ResponsesAgentStreamEvent, None]:
+    # obo_workspace_client = get_obo_workspace_client()
+    agent = await init_agent()
+    messages = {"messages": to_chat_completions_input([i.model_dump() for i in request.input])}
 
-        async for event in result.stream_events():
-            if event.type == "raw_response_event":
-                yield event.data.model_dump()
-            elif (
-                event.type == "run_item_stream_event" and event.item.type == "tool_call_output_item"
-            ):
-                yield ResponsesAgentStreamEvent(
-                    type="response.output_item.done",
-                    item=event.item.to_input_item(),
-                )
+    async for event in agent.astream(input=messages, stream_mode=["updates", "messages"]):
+        if event[0] == "updates":
+            for node_data in event[1].values():
+                if len(node_data.get("messages", [])) > 0:
+                    for item in output_to_responses_items_stream(node_data["messages"]):
+                        yield item
+        # filter the streamed messages to just the generated text messages
+        elif event[0] == "messages":
+            try:
+                chunk = event[1][0]
+                if isinstance(chunk, AIMessageChunk) and (content := chunk.content):
+                    yield ResponsesAgentStreamEvent(
+                        **create_text_delta(delta=content, item_id=chunk.id)
+                    )
+            except Exception as e:
+                print(e)
